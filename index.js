239f9b9fef6145cf5161769d5196250c62035692
James Snell <vulpes3.1415@yahoo.com>
2023-05-18 14:46:44 -0600

    phase-0-intro-to-js-2-array-lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+    return cats;
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+    return cats;
+}
+
+function appendCat(name) {
+    var cats2 = [...cats, name]
+    return cats2;
+}
+
+function prependCat(name) {
+    var cat3 = [name, ...cats];
+    return cat3;
+}
+
+function removeLastCat() {
+    var cat4 = cats.slice(0, cats.length-1);
+    return cat4;
+}
+
+function removeFirstCat() {
+    var cat5 = cats.slice(1);
+    return cat5;
+}
